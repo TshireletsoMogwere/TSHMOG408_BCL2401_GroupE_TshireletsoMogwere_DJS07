b6b2f76e2a4c87134e7f539f735f133bdc3cb33c
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-29 20:21:04 +0200

    generated random selection of images

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,16 +1,29 @@
+import React from "react"
+import memesData from "./memesData"
 
  
  function Meme() {
-    return (
+// Creates memeImage state
+const [imageMeme, setImageMeme] = React.useState("")
+ 
+function getImageMeme() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    setImageMeme(memesArray[randomNumber].url)
+}
+return (
         // Creates a form component
         <main>
                 <div className="form">
                 <input type="text" id="top-text" className="form--input" placeholder="Top text"/>
                 <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button">Get a new meme image 🖼️</button>
+                <button onClick={getImageMeme}className="form--button">Get a new meme image 🖼️</button>
+                <img src={imageMeme} className="meme--image"/>
                 </div>
         </main>
     )
 }
+ 
+ 
 
 export default Meme
