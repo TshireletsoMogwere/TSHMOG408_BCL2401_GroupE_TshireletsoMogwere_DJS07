ec8d54ce1faea541984fc85a45ff668f32446da4
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-30 21:42:40 +0200

    inserted header image

--- Meme Generator/src/components/Header.jsx
+++ Meme Generator/src/components/Header.jsx
@@ -4,10 +4,10 @@ function App() {
     return (
       // Creates header component
       <header className="header">
+        <img src="./images/troll-face.png.png" className="header--image"/>
        <h2 className="header--title">Meme Generator</h2>
        <h4 className="header--project">React Course - Project 3</h4>
-       <img src="" className="header--image"/>
-      </header>
+       </header>
     )
   }
   
