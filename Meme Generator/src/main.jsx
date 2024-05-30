9fea422dc01d0a711582eb02553cc8e78a163f2a
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-30 21:25:05 +0200

    removed duplicate

--- Meme Generator/src/main.jsx
+++ Meme Generator/src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Meme from './Meme.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Meme />
   </React.StrictMode>,
 )
