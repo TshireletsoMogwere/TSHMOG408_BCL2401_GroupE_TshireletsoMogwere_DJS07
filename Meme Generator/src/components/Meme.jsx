2e4dd6e873b755981bd00797aa93c9157f571342
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-30 21:27:54 +0200

    updated function meme

--- Meme Generator/src/components/Meme.jsx
+++ Meme Generator/src/components/Meme.jsx
@@ -4,25 +4,38 @@ import memesData from "../memesData"
  
  function Meme() {
 // Creates memeImage state
-const [imageMeme, setImageMeme] = React.useState("http://i.imgflip.com/1bij.jpg")
+const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg" 
+})
+const [allMemes, setAllMemes] = React.useState([])
+
+React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+}, [])
+
+// const [imageMeme, setImageMeme] = React.useState("http://i.imgflip.com/1bij.jpg")
  
-function getImageMeme() {
-    // Selects random images from memes array
-    const memesArray = memesData.data.memes
-    const randomNumber = Math.floor(Math.random() * memesArray.length) 
-    setImageMeme(memesArray[randomNumber].url)
-}
-return (
-        // Creates a form component
-        <main>
-                <div className="form">
-                <input type="text" id="top-text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button onClick={getImageMeme}className="form--button">Get a new meme image 🖼️</button>
-                <img src={imageMeme} className="meme--image"/>
-                </div>
-        </main>
-    )
+// function getImageMeme() {
+//     // Selects random images from memes array
+//     const memesArray = memesData.data.memes
+//     const randomNumber = Math.floor(Math.random() * memesArray.length) 
+//     setImageMeme(memesArray[randomNumber].url)
+// }
+// return (
+//         // Creates a form component
+//         <main>
+//                 <div className="form">
+//                 <input type="text" id="top-text" className="form--input" placeholder="Top text"/>
+//                 <input type="text" className="form--input" placeholder="Bottom text"/>
+//                 <button onClick={getImageMeme}className="form--button">Get a new meme image 🖼️</button>
+//                 <img src={imageMeme} className="meme--image"/>
+//                 </div>
+//         </main>
+//     )
 }
  
  
